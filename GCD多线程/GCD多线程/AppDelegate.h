a689528ec1ec76359e7eee3b96135aad7207f837
TT <TT@MbPro.local>
2018-03-02 15:01:33 +0800

    GCD

---
+++ GCD多线程/GCD多线程/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD多线程
+//
+//  Created by TT on 2018/3/2.
+//  Copyright © 2018年 T AO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD多线程/GCD多线程/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD多线程
+//
+//  Created by TT on 2018/3/2.
+//  Copyright © 2018年 T AO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
