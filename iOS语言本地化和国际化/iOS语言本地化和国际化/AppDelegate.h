fb49f662bcca25f583f6e4f64c5911e0d6a96216
TT <TT@MbPro.local>
2018-03-02 14:13:58 +0800

    no message

---
+++ iOS语言本地化和国际化/iOS语言本地化和国际化/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS语言本地化和国际化
+//
+//  Created by TT on 2018/3/2.
+//  Copyright © 2018年 T AO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS语言本地化和国际化/iOS语言本地化和国际化/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS语言本地化和国际化
+//
+//  Created by TT on 2018/3/2.
+//  Copyright © 2018年 T AO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
