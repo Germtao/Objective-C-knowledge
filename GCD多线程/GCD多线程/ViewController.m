fcc5351f3ff4f6d2f9f50bc258b1f1d723cee12a
TT <TT@MbPro.local>
2018-03-07 21:38:04 +0800

    no message

--- GCD多线程/GCD多线程/ViewController.m
+++ GCD多线程/GCD多线程/ViewController.m
@@ -16,13 +16,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//    [self deadlock];
+    
+//    [self group];
+    
+    [self groupWait];
 }
 
+// MARK: - GCD 分组任务
+- (void)groupWait {
+    // 创建线程组
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_queue_t serialQueue = dispatch_queue_create("serialQueue", DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_group_async(group, serialQueue, ^{
+        for (int i = 0; i < 2; i++) {
+            NSLog(@"group-serial - %@", [NSThread currentThread]);
+        }
+    });
+    
+    //
+    NSLog(@"%ld", dispatch_group_wait(group, DISPATCH_TIME_NOW));
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)group {
+    
+    // 创建线程组
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_queue_t serialQueue = dispatch_queue_create("serialQueue", DISPATCH_QUEUE_SERIAL);
+    
+    // 组异步执行
+    dispatch_group_async(group, serialQueue, ^{
+        for (int i = 0; i < 2; i++) {
+            NSLog(@"group-serial - %@", [NSThread currentThread]);
+        }
+    });
+    
+    dispatch_group_async(group, serialQueue, ^{
+        for (int i = 0; i < 3; i++) {
+            NSLog(@"group-02-serial - %@", [NSThread currentThread]);
+        }
+    });
+    
+    // 把第三个参数block传入第二个参数队列中去。
+    // 而且可以保证第三个参数block执行时，group中的所有任务已经全部完成
+    dispatch_group_notify(group, serialQueue, ^{
+        NSLog(@"Finished - %@", [NSThread currentThread]);
+    });
+}
+
+// MARK: - GCD 死锁问题
+- (void)deadlock {
+    
+    dispatch_queue_t mainQueue = dispatch_get_main_queue();
+    
+    id block = ^{
+        NSLog(@"%@", [NSThread currentThread]);
+    };
+    
+    // 向当前串行队列中同步派发一个任务, 死锁
+//    dispatch_sync(mainQueue, block);
+    dispatch_async(mainQueue, block);
 }
 
 
