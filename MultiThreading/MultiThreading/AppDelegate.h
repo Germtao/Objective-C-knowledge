30156b05c33580b1613b3c4f3f73e512a4a68882
TT <TT@MbPro.local>
2018-04-01 22:05:34 +0800

    no message

---
+++ MultiThreading/MultiThreading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiThreading
+//
+//  Created by TT on 2018/4/1.
+//  Copyright © 2018年 T AO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
