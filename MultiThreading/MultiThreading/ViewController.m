5230fb741cdcd1a269eab6dfbf01ad907af7ce59
TT <TT@MbPro.local>
2018-04-04 13:43:43 +0800

    转场方式

--- MultiThreading/MultiThreading/ViewController.m
+++ MultiThreading/MultiThreading/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "NSThreadController.h"
+#import "NSOperationController.h"
+#import "GCDController.h"
 
 @interface ViewController ()
 
@@ -16,7 +19,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
@@ -25,5 +28,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+//- (IBAction)ThreadAction:(UIButton *)sender {
+
+//    NSThreadController *thread = [[NSThreadController alloc] init];
+//    thread.title = @"NSThread";
+//    [self.navigationController pushViewController:thread animated:YES];
+//}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    if ([segue.identifier isEqualToString:@"showThread"]) {
+        NSThreadController *thread = segue.destinationViewController;
+        thread.title = @"NSThread";
+    }
+    else if ([segue.identifier isEqualToString:@"showOperation"]) {
+        NSOperationController *operation = segue.destinationViewController;
+        operation.title = @"NSOperation";
+    }
+}
+
+//- (IBAction)NSOperationAction:(UIButton *)sender {
+//
+//    NSOperationController *operation = [[NSOperationController alloc] init];
+//    operation.title = @"NSOperation";
+//    [self.navigationController pushViewController:operation animated:YES];
+//}
+
+- (IBAction)GCDAction:(UIButton *)sender {
+    
+    GCDController *gcd = [[GCDController alloc] init];
+    gcd.title = @"GCD";
+    [self.navigationController pushViewController:gcd animated:YES];
+}
 
 @end
