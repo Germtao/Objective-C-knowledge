8c0744790e52c6a8d244f40ec3c0edc391506f09
QDSG <piftao1127@163.com>
2019-06-14 15:54:02 +0800

    runloop

---
+++ RunLoop/RunLoop/RunLoop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunLoop
+//
+//  Created by QDSG on 2019/6/14.
+//  Copyright © 2019 unitTao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
